99da8e79268a3a4ec4679bb3601216a381246afd
Flash-Royal <flash.royal473@gmail.com>
2021-04-22 17:36:28 +0300

    model and serializer for GameGenre

--- firstSite/catalog/models.py
+++ firstSite/catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class GameGenre(models.Model):
+    idName = models.CharField(max_length = 100, help_text = "Enter Game Genre url id")
+    name = models.CharField(max_length = 100, help_text = "Enter Game Genre name")
+    description = models.TextField(max_length = 1000, help_text = "Description about game genre")
+
+    def getLink(self):
+        return reverse('GenreDetail', args = [str(self.idName)])
--- firstSite/catalog/views.py
+++ firstSite/catalog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from rest_framework import generics
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.views import APIView
+
+from .serializers import *
+
+def main(request):
+    return render(request,'main.html')
 
 # Create your views here.
