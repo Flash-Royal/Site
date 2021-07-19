ab61f741e661b8efcd3a503f93ffd4a046690bf5
Flash-Royal <flash.royal473@gmail.com>
2021-07-19 07:35:15 +0300

    delete if in templeFurn and add him in views

--- secondSite/catalog/views.py
+++ secondSite/catalog/views.py
@@ -21,12 +21,44 @@ class FurnitureDetail(APIView):
     renderer_classes = [MultiPartParser, TemplateHTMLRenderer]
     template_name = 'templeFurn.html'
 
+    def changeNewID(self, mass, index):
+        for item in mass:
+            if item.newID >= index:
+                item.newID += 1
+
+    def parseData(self, mass):
+        grid1 = []
+        grid2 = []
+        if len(mass) < 4:
+            for i in range(len(mass)):
+                if (i < (len(mass) // 2 + len(mass) % 2)):
+                    grid1.append(list(mass)[i])
+                else:
+                    grid2.append(list(mass)[i])
+        else:
+            for i in range(len(mass)):
+                if (i < (len(mass) - 2)):
+                    grid1.append(list(mass)[i])
+                else:
+                    grid2.append(list(mass)[i])
+
+        for id, item in enumerate(grid1):
+            item.newID = id
+
+        for id, item in enumerate(grid2):
+            item.newID = id
+
+        return grid1, grid2
     def get(self, request, furniture):
+        arr = []
         furns = Furniture.objects.all()
         furn = Furniture.objects.values('id','name','idName').get(idName = furniture)
         images = Images.objects.filter(nameFurniture = furn['id'])
         texts = Texts.objects.filter(nameFurniture = furn['id'])
-        return Response({'furns' : furns, 'furn' : furn, "images" : images, 'texts' : texts})
+        grid1Images, grid2Images = self.parseData(images)
+        grid1Texts, grid2Texts = self.parseData(texts)
+
+        return Response({'furns' : furns, 'furn' : furn, 'gr1Text' : grid1Texts, 'gr2Text' : grid2Texts, 'gr1Im' : grid1Images, 'gr2Im' : grid2Images})
 
 
 # Create your views here.
