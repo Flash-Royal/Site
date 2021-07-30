e3625ded57d9f1e92b45c7df7bf56499216a171b
Flash-Royal <flash.royal473@gmail.com>
2021-07-30 06:46:52 +0300

    download instagram stories

--- secondSite/secondSite/settings.py
+++ secondSite/secondSite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 CORS_ORIGIN_ALLOW_ALL=True
@@ -123,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+django_heroku.settings(locals())
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
--- secondSite/test/test.py
+++ secondSite/test/test.py
@@ -1,11 +1,15 @@
-# import requests
-# from bs4 import BeautifulSoup
-#
-#
-# url = "https://www.instagram.com/romanov_steel_and_wood/"
-#
-# response = requests.get(url)
-#
-# soap = BeautifulSoup(response.text, "html.parser")
-# # print(soap)
-# soap.find_all("a")
+import instaloader
+from instaloader import Profile, Post
+
+instance = instaloader.Instaloader(save_metadata = False, compress_json = False)
+
+login = "vlad.popov471@gmail.com"
+password = "python1"
+
+instance.login(user = login, passwd = password)
+
+profile = Profile.from_username(instance.context, username = "romanov_steel_and_wood")
+
+for highlight in instance.get_highlights(user = profile):
+    for item in highlight.get_items():
+        instance.download_storyitem(item, '{}'.format(highlight.title))
