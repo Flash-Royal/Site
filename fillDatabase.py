c49a4f6c915402c56df995e17bd1becaf375e957
Flash-Royal <flash.royal473@gmail.com>
2021-07-31 01:43:01 +0300

    fill database for furniture from instagram

--- fillDatabase.py
+++ fillDatabase.py
@@ -1,22 +1,84 @@
 import os
 import sqlite3
 
-names = []
+class FurnDataBase():
+    def __init__(self, nameDB):
+        self.conn = sqlite3.connect(nameDB)
 
-for object in os.listdir('photos'):
-    if os.path.isdir('photos/{}'.format(object)):
-        print(object)
-        names.append(object)
+    def findNewNames(self):
+        newNames = []
+        names = []
+        existingNames = []
 
-name = 'chair'
-conn = sqlite3.connect("db.sqlite3")
-cur = conn.cursor()
-sql = "Select id From catalog_furniture where idname = '{}'".format(name)
-# sqlTest = """Insert into catalog_images(nameFurniture, image) values(2, 'furniture/zhurnalniy-stolik2.jpg');"""
+        for object in os.listdir('furniture'):
+            if os.path.isdir('furniture/{}'.format(object)):
+                names.append(object)
+
+        cur = self.conn.cursor()
+        sql = "select name from catalog_furniture"
+        cur.execute(sql)
+
+        res = cur.fetchall()
+        for row in res:
+            if row[0] != '':
+                existingNames.append(row[0])
+
+        for name in names:
+            if name not in existingNames:
+                newNames.append(name)
+
+        return newNames
+
+    def createNewIdName(self):
+        names = self.findNewNames()
+        idNames = {}
+
+        print("Enter idNames for new names")
+        for name in names:
+            idNames[name] = str(input("{}: ".format(name)))
+
+        return names, idNames
+
+    def addNewNames(self):
+        names, idNames = self.createNewIdName()
+        image = {}
+        cur = self.conn.cursor()
+        for name in names:
+            for object in os.listdir('furniture/{}'.format(name)):
+                if os.path.isfile('furniture/{}/{}'.format(name, object)):
+                    image[name] = 'furniture/{}/{}'.format(name, object)
+                    break
+            sql = "insert into catalog_furniture(idName,name,imageBack) values('{}', '{}', '{}')".format(idNames[name], name, image[name])
+            cur.execute(sql)
+            self.conn.commit()
+
+furniture = FurnDataBase("db.sqlite3")
+furniture.addNewNames()
+
+# names = []
+# images = {}
+# for object in os.listdir('furniture'):
+#     if os.path.isdir('furniture/{}'.format(object)):
+#         names.append(object)
+#
+# print(names)
+#
+# for name in names:
+#     files = []
+#     for object in os.listdir('furniture/{}'.format(name)):
+#         if os.path.isfile('furniture/{}/{}'.format(name, object)):
+#             files.append(object)
+#     images[name] = files
+#
+# print(images)
+# conn = sqlite3.connect("db.sqlite3")
+# cur = conn.cursor()
+# sql = "Select id, name, idName, imageBack From catalog_furniture where idname = '{}'".format(name)
+# sql1 = "Select nameFurniture_id from catalog_images"
+# cur.execute(sql1)
+# res = cur.fetchall()
+# print(res[0])
+
+# sqlTest = "Insert into catalog_images(nameFurniture_id, image) values((Select id From catalog_furniture where idname = '{}'), 'furniture/Стулья/2020-10-04_19-52-20_UTC.jpg');".format(name)
 # cur.execute(sqlTest)
 # conn.commit()
-cur.execute(sql)
-res = cur.fetchall()
-for row in res:
-    print(type(row))
-    print(row[0])
